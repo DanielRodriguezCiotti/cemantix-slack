ebdcc61fe572fdd8d5679a2faaf895eaf986dbcd
Daniel Rodriguez <darc2203@gmail.com>
2023-10-24 14:32:40 +0200

    debug: test

--- index.ts
+++ index.ts
@@ -3,7 +3,5 @@ import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 async function handler(_req: Request): Promise<Response> {
   const body = await _req.json();
   const body_string = JSON.stringify(body);
-  return new Response(body_string);
+  return new Response("body_string");
 }
-
-serve(handler);
