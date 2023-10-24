dcb190be5b300ba66a7d26367be9359705d88eb2
Daniel Rodriguez <darc2203@gmail.com>
2023-10-24 14:24:50 +0200

    debug: log request body

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+  const body = await _req.json();
+  const body_string = JSON.stringify(body);
+  return new Response(body_string);
 }
 
 serve(handler);
